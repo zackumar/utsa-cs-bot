4b854b93302501da0abecfd56156b1012e2db13a
Zack Umar <zackumar@gmail.com>
2021-12-20 17:01:03 +0000

    More archive checks

--- src/events/member_left_event.py
+++ src/events/member_left_event.py
@@ -2,6 +2,9 @@ from events.event import Event
 
 import logging
 
+from slack_sdk.errors import SlackApiError
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -13,14 +16,21 @@ class MemberLeftEvent(Event):
         if self.bot.remove_all == True:
             return
 
-        if not self.bot.employee_list.loc[
-            self.bot.employee_list["user_id"] == event["user"]
-        ].empty:
-            logger.info(
-                "{0} was removed from {1}. Adding them back in.".format(
-                    event["user"], event["channel"]
+        try:
+
+            if not self.bot.employee_list.loc[
+                self.bot.employee_list["user_id"] == event["user"]
+            ].empty:
+                logger.info(
+                    "{0} was removed from {1}. Adding them back in.".format(
+                        event["user"], event["channel"]
+                    )
                 )
-            )
-            self.bot.app.client.conversations_invite(
-                channel=event["channel"], users=event["user"]
-            )
+                self.bot.app.client.conversations_invite(
+                    channel=event["channel"], users=event["user"]
+                )
+
+        except SlackApiError as e:
+            if e.response["error"] == "is_archived":
+                return
+            logger.error(e)
